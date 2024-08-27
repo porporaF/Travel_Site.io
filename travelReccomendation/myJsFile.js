45059987ce07403df95de94069df032e873dcc16
Francesco Porpora <porpora.f@alice.it>
2024-08-27 09:36:04 -0400

    progress

---
+++ travelReccomendation/myJsFile.js
@@ -0,0 +1,15 @@
+const BookedTravels [];
+
+function searchClear ()
+{
+    document.getElementById("searchField").value="";
+}
+
+function BookNow ()
+{
+    
+}
+
+document.getElementById("btnClear").addEventListener("click",searchClear);
+
+document.getElementById("btnBook").addEventListener("click",BookNow);
