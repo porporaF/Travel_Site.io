f35e9cb4ef47d2fadc3c23ff3724785b749cbf45
Francesco Porpora <porpora.f@alice.it>
2024-08-29 10:12:29 -0400

    test

--- travelReccomendation/myJsFile.js
+++ travelReccomendation/myJsFile.js
@@ -12,18 +12,30 @@ function BookNow ()
 
 function displaySearch(){
     const city = document.getElementById('searchField').value;
+    let countriesArray;
+    let TemplesArray;
+    let beachesArray;
+
 const jsonUrl ='./travel_recommendation_api.json';
     fetch(jsonUrl)
-    .then (response=>{
-        return response.json();    
-    })
-    .then (data =>{
-        console.log('Data from API: ',data);
-    })
-    .catch (error =>{
-        console.error(('An error occured:',error))
-    });
-}
+    .then (response=> response.json())
+ .then(data => {
+console.log(typeof data);
+const jsonData= JSON.stringify(data);
+console.log(typeof jsonData);
+console.log(jsonData);
+
+const jsonDataName= JSON.stringify(data,['beaches']);
+console.log(typeof jsonDataName);
+console.log(JSON.stringify(jsonDataName),['name']);
+
+
+        
+         }
+ 
+   )
+    
+ }
 
 document.getElementById("btnClear").addEventListener("click",searchClear);
 // document.getElementById("btnBook").addEventListener("click",BookNow);
