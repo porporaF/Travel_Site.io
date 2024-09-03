8e3116a2ae47ce199eae207ab7835ce3e76e844b
Francesco Porpora <porpora.f@alice.it>
2024-09-03 13:47:03 -0400

    quasi finito

--- myJsFile.js
+++ myJsFile.js
@@ -21,17 +21,26 @@ const jsonUrl ='./travel_recommendation_api.json';
         console.log()
         for (let i=0;i<Object.keys(data['countries']).length;i++)
         {
-            console.log(data['countries'][i].name);
+
         if (data['countries'][i].name.includes(city)){
-        console.log('ok');
+
+         for (let j=0;j<Object.keys(data['countries'][i].cities).length;j++) {  
+            var divElement =document.getElementById('searchList');
+            var ulElement=document.createElement('ul');
+            var liElement=document.createElement('li');
+            var TextElement=document.createElement(data['countries'][i].name);
+            var imgElement=document.createElement('img');
+
+            console.log(data['countries'][i].cities[i].imageUrl);
+         }
+            //liElement.appendChild
+            //let img = document.createElement('img');
+            //img.src =data['countries'][i].imgUrl;
+        }
         }
 
-                    }
-                }
-       
-         )
-
-                        }
+                    })
+                           }
     
 
 document.getElementById("btnClear").addEventListener("click",searchClear);
