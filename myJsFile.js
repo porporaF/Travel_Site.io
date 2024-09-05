a6116a3d8bb6a77b937280849330d8e64d8c0143
Francesco <porpora.f@alice.it>
2024-09-05 05:13:58 -0400

    ok

--- myJsFile.js
+++ myJsFile.js
@@ -15,34 +15,37 @@ function displaySearch(){
     const searchValue = document.getElementById('searchField').value;
    
        
-    if (searchValue.includes('beaches')){
 
 const jsonUrl ='./travel_recommendation_api.json';
     fetch(jsonUrl)
     .then (response=> response.json())
     .then(data => {
-               for (let i=0;i<Object.keys(data['beaches']).length;i++)
+        var divElement =document.getElementById('searchList');
+           divElement.innerHTML='';
+               for (let i=0;i<Object.keys(data[searchValue]).length;i++)
         {
            
-          
-           var ScityName=data['beaches'][i].name;
-           var imgUrl=data['beaches'][i].imageUrl;
+           var ScityName=data[searchValue][i].name;
+           var imgUrl=data[searchValue][i].imageUrl;
+           var description=data[searchValue][i].description;
            console.log('Prima della funzione',imgUrl);
            console.log('Prima della funzione CityName:',ScityName);
            
-           fillDiv(ScityName,imgUrl);
+           fillDiv(ScityName,imgUrl,description);
         }
            
                  })
         
-                                      }
+                                      
                         }
 
-function fillDiv (cityName,url){
+function fillDiv (cityName,url,description){
             var divElement =document.getElementById('searchList');
+        
             var ulElement=document.createElement("ul");
             var liElement=document.createElement("li");
             var TextElement=document.createTextNode(cityName);
+            var description=document.createTextNode(description);
             console.log('Dopo la funzione',cityName);
             var imgElement=document.createElement("img");
             imgElement.src=url;
@@ -55,8 +58,12 @@ function fillDiv (cityName,url){
             liElement.appendChild(breakElement);
             liElement.appendChild(imgElement);
             liElement.appendChild(breakElement);
-             ulElement.appendChild(liElement);
-             liElement.appendChild(breakElement);
+            liElement.appendChild(description);
+            liElement.appendChild(breakElement);
+            
+            ulElement.appendChild(liElement);
+            liElement.appendChild(breakElement);
+            
             liElement.appendChild(btnView);
             liElement.appendChild(breakElement);
             ulElement.appendChild(liElement)
